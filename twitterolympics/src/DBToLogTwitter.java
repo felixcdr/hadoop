fa2518f697135cfd13ebc34090a9d673f6724f4a
Félix Cuadrado <felix.cdr@gmail.com>
2013-02-25 15:54:40 +0000

    initial version

---
+++ twitterolympics/src/DBToLogTwitter.java
@@ -0,0 +1,83 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.compress.DefaultCodec;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.db.DBConfiguration;
+import org.apache.hadoop.mapreduce.lib.db.DBInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+public class DBToLogTwitter {
+
+	static enum Counters {
+		FaultyEntries
+	};
+
+	public static void runJob(String mysqlJar, String output) throws Exception {
+
+		Configuration conf = new Configuration();
+
+		JobHelper.addJarForJob(conf, mysqlJar);
+
+		Job job = new Job(conf);
+
+		DBConfiguration.configureDB(conf, "com.mysql.jdbc.Driver",
+				"jdbc:mysql://quark.student.eecs.qmul.ac.uk/tridec01"
+						+ "?user=olympicro&password=olympicro");
+
+		job.setJarByClass(DBToLogTwitter.class);
+
+		job.setMapperClass(Map.class);
+
+		job.setNumReduceTasks(0);
+
+		job.setInputFormatClass(DBInputFormat.class);
+
+		job.setOutputKeyClass(Tweet.class);
+		job.setOutputValueClass(NullWritable.class);
+
+		job.setOutputFormatClass(SequenceFileOutputFormat.class);
+		SequenceFileOutputFormat.setCompressOutput(job, true);
+		SequenceFileOutputFormat.setOutputCompressionType(job,
+		SequenceFile.CompressionType.BLOCK);
+		SequenceFileOutputFormat.setOutputCompressorClass(job,
+		DefaultCodec.class);
+		
+		Path outputPath = new Path(output);
+
+		FileOutputFormat.setOutputPath(job, outputPath);
+
+		outputPath.getFileSystem(conf).delete(outputPath, true);
+		
+		DBInputFormat.setInput(
+		        job,
+		        TweetRecord.class,
+		        "select * from T07272039BASELINEMerged limit 200",
+		        "SELECT COUNT(id) FROM T07272039BASELINEMerged");
+		
+		
+		job.waitForCompletion(true);
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		runJob(args[0], args[1]);
+	}
+
+	public static class Map extends
+			Mapper<LongWritable, TweetRecord, Tweet, NullWritable> {
+
+		public void map(LongWritable key, TweetRecord value, Context context)
+				throws IOException, InterruptedException {
+			context.write((Tweet) value, NullWritable.get());
+		}
+
+	}
+
+}
---
+++ twitterolympics/src/TweetRecord.java
@@ -0,0 +1,61 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.lib.db.DBWritable;
+
+public class TweetRecord extends Tweet implements Writable, DBWritable {
+
+	private final static SimpleDateFormat sdf = new SimpleDateFormat(
+			"yyyy-MM-dd, hh:mm:ss");
+
+	public static String[] fields = { "createdAt", "geoLocationLat",
+			"geoLocationLong", "placeInfo", "id", "tweet", "source", "lang",
+			"screenName", "replyTo", "rtCount", "hashtags" };
+
+
+	@Override
+	public void readFields(ResultSet resultSet) throws SQLException {
+		try {
+
+			setCreatedAt(sdf.parse(resultSet.getString("createdAt")));
+
+		} catch (ParseException e) {
+			throw new SQLException("Failed to convert String to date", e);
+		}
+		setGeoLocationLat(resultSet.getDouble("geoLocationLat"));
+		setGeoLocationLong(resultSet.getDouble("geoLocationLong"));
+		setPlaceInfo(resultSet.getString("placeInfo"));
+		setId(resultSet.getLong("id"));
+		setTweet(resultSet.getString("tweet"));
+		setSource(resultSet.getString("source"));
+		setLang(resultSet.getString("lang"));
+		setScreenName(resultSet.getString("screenName"));
+		setReplyTo(resultSet.getString("replyTo"));
+		setRtCount(resultSet.getInt("rtCount"));
+		setHashtags(resultSet.getString("hashtags"));
+	}
+
+	@Override
+	public void write(PreparedStatement statement) throws SQLException {
+		/*int idx = 1;
+		statement.setString(idx++, getSymbol());
+		try {
+			statement.setDate(idx++, new Date(sdf.parse(getDate()).getTime()));
+		} catch (ParseException e) {
+			throw new SQLException("Failed to convert String to date", e);
+		}
+		statement.setDouble(idx++, getOpen());
+		statement.setDouble(idx++, getHigh());
+		statement.setDouble(idx++, getLow());
+		statement.setDouble(idx++, getClose());
+		statement.setInt(idx++, getVolume());
+		statement.setDouble(idx, getAdjClose());*/
+	}
+
+	
+	
+}
